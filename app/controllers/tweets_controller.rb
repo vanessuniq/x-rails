5685e009f83178ace73818050c199746426d89cf
Vanessa Fotso <vanessuniq@gmail.com>
2025-11-05 15:40:49 -0500

    Add like and retweet simple functionalities
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :find_tweet, only: %i[ like retweet]
   def index
     @tweets = Tweet.all.order(created_at: :desc)
     @tweet = Tweet.new
@@ -13,9 +14,28 @@ class TweetsController < ApplicationController
     end
   end
 
+  def like
+    @tweet.increment(:likes_count).save
+
+    redirect_to tweets_path
+  end
+
+  def retweet
+    @tweet.increment(:retweet_count).save
+
+    redirect_to tweets_path
+  end
+
   private
 
   def tweet_params
     params.expect(tweet: [ :body ])
+  end
+
+  def find_tweet
+    @tweet = Tweet.find_by(id: params[:tweet_id])
+    if !@tweet
+      redirect_to tweets_path, alert: 'Cannot process action: Tweet not found.'
+    end
   end
 end
