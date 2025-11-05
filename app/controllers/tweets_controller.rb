d033c9685e2d20b565f5c28ae0281a1dbacc178f
Vanessa Fotso <vanessuniq@gmail.com>
2025-11-05 13:27:09 -0500

    Add tweet controller index and create actions
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,21 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to posts_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.expect(tweet: [ :body ])
+  end
+end
